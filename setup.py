d523b8b8be2b6de1f8560fd5b07956886d80e262
Willi Stepp <willi.stepp@epfl.ch>
2022-08-03 14:14:48 +0200

    setup for installation in MM folder

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pymm_eventserver",
-    version="0.1.03",
+    version="0.1.4",
     description="Micro-Manager PythonEventServer",
     long_description=long_description,
     long_description_content_type="text/markdown",
